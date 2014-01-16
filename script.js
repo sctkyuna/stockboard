b0743da6dd169f12117cd8d55a5bd6579971d5dc
Stella Cho <sctkyuna@new-host-3.home>
2014-01-16 03:40:06 -0500

    under construction

---
+++ script.js
@@ -0,0 +1,42 @@
+var getStock = function(company) {
+	// eventually this will query the server for the price
+
+    /*
+    if (company exists)
+        get stock value
+        return stock
+    else
+        return false
+    */
+	return 34;
+}
+
+
+$(document).ready(function() {
+    $("#addButton").click(function() {
+        var stockID = $('input[name=stockID]').val();
+        stockValue = getStock(stockID);
+        if (stockValue) {
+        	var stockhtml = _.template("<div class=stock id=chart_<%sid%>><h3><%sid%></h3><p><%value%></p></div>", )
+            $("body").append("<div class='stock' id=chart_" + stockID + "><h3>" + stockID + "</h3><p>" + stockValue + "</p></div>");
+            $("#chart_" + stockID).draggable();
+        }
+        else {
+            alert(stockID + " is not a valid stockID");
+        }
+    });
+
+    $("#addButton").mouseover(function() {
+        $(this).fadeTo("fast",1);
+    });
+    
+    $("#addButton").mouseout(function() {
+        $(this).fadeTo("fast",0.5); 
+    });
+    /*
+    $(document).on("click", ".stock", function(){
+        $(".stock").draggable();
+    } );
+    */ 
+
+});
